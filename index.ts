43cc4262bc95ec8b31cee1bb26826d15ecc01376
Emils Bernhards <bernhardsemils@gmail.com>
2021-09-16 21:08:48 +0200

    Added comments and fixed remote file check.

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import {DateTime} from "luxon"
 import shell from "shelljs"
 import * as path from "path";
-import * as console from "console";
 require('dotenv').config()
 
 const currentDate: string = DateTime.now().toFormat("dd-MM-yyyy")
 const fileName: string = `${currentDate}.sql`
 
+/**
+ * Generates the backup sql file.
+ */
 const pgDumpOutput = shell.exec(`pg_dump -U ${process.env.PG_USER} ${process.env.PG_DATABASE} > ${fileName}`)
 
 if (pgDumpOutput.code !== 0) {
@@ -14,13 +16,23 @@ if (pgDumpOutput.code !== 0) {
     shell.exit(1)
 }
 
+/**
+ * Checks whether the remote backup host already has the backup that is currently being backed up.
+ * This prevents accidentally overriding a legitimate backup.
+ */
 const checkIfRemoteFileExistsOutput = shell.exec(`ssh ${process.env.BACKUP_MACHINE_USER}@${process.env.BACKUP_MACHINE_IP}  test -f  ${process.env.BACKUP_MACHINE_ABSOLUTE_PATH + "/" +fileName}`)
 
-if (checkIfRemoteFileExistsOutput.code !== 0) {
-    shell.echo("check remote file command failed. Error: " + " " + checkIfRemoteFileExistsOutput.stderr + " Output: " + " " + checkIfRemoteFileExistsOutput.stdout)
+if (checkIfRemoteFileExistsOutput.code === 0) {
+    shell.echo(`file ${fileName} already exists on host ${process.env.BACKUP_MACHINE_IP} user ${process.env.BACKUP_MACHINE_USER}`)
+    shell.exit(1)
+} else if (checkIfRemoteFileExistsOutput.code === 2) {
+    shell.echo("ssh command failed. Error: " + " " + checkIfRemoteFileExistsOutput.stderr + " Output: " + " " + checkIfRemoteFileExistsOutput.stdout)
     shell.exit(1)
 }
 
+/**
+ * Syncs the backup to the remote backup host.
+ */
 const rsyncOutput = shell.exec(`rsync ${path.join(__dirname, fileName)} ${process.env.BACKUP_MACHINE_USER}@${process.env.BACKUP_MACHINE_IP}:${process.env.BACKUP_MACHINE_ABSOLUTE_PATH}`)
 
 if (rsyncOutput.code !== 0) {
@@ -28,7 +40,9 @@ if (rsyncOutput.code !== 0) {
     shell.exit(1)
 }
 
-
+/**
+ * Moves the local backup out of the project directory into the some backup directory.
+ */
 const moveBackupOutput = shell.exec(`mv ${fileName} ${process.env.LOCAL_BACKUP_RELATIVE_PATH}`)
 
 if (moveBackupOutput.code !== 0) {
