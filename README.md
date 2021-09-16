c28d05ac1f13c32f132bdc0ab1d9f001da957bd1
Emils Bernhards <bernhardsemils@gmail.com>
2021-09-16 20:28:34 +0200

    .

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 
 1. You must create a `.pgpass` file in the home directory of the user who will be running the cron job.
    In this file, you must add the following line `hostname:port:database:username:password`. This will
-   authenticate any Postgres commands done through the shell.  
+   authenticate any Postgres commands done through the shell. After creating the file, you must change the
+   file permissions by running `chmod 600 ~/.pgpass`.
 2. You must create an `.env` file. See `.env.example` for what to include.
--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
 import {DateTime} from "luxon"
 import shell from "shelljs"
+import * as path from "path";
 require('dotenv').config()
 
-let currentDate = DateTime.now().toFormat("dd-MM-yyyy")
+const currentDate: string = DateTime.now().toFormat("dd-MM-yyyy")
+const fileName: string = `${currentDate}.sql`
 
-shell.exec(`pg_dump -U ${process.env.PG_USER} ${process.env.PG_DATABASE} > ${currentDate}.sql`)
+const pgDumpOutput = shell.exec(`pg_dump -U ${process.env.PG_USER} ${process.env.PG_DATABASE} > ${fileName}`)
+
+if (pgDumpOutput.code !== 0) {
+    shell.echo("pg_dump command failed. Error: " + " " + pgDumpOutput.stderr + " Output: " + " " + pgDumpOutput.stdout)
+    shell.exit(1)
+}
+
+
+const rsyncOutput = shell.exec(`rsync ${path.join(__dirname, fileName)} ${process.env.BACKUP_MACHINE_USER}@${process.env.BACKUP_MACHINE_IP}:${process.env.BACKUP_MACHINE_ABSOLUTE_PATH}`)
+
+if (rsyncOutput.code !== 0) {
+    shell.echo("rsync command failed. Error: " + " " + rsyncOutput.stderr + " Output: " + " " + rsyncOutput.stdout)
+    shell.exit(1)
+}
