f9ac7a669fa92c38abdad2e0c187a34b370a5c22
Emils Bernhards <bernhardsemils@gmail.com>
2021-09-17 18:32:32 +0200

    Removed checking for remote file existance

--- index.ts
+++ index.ts
@@ -17,25 +17,11 @@ if (pgDumpOutput.code !== 0) {
 }
 
 /**
- * Checks whether the remote backup host already has the backup that is currently being backed up.
- * This prevents accidentally overriding a legitimate backup.
- */
-const checkIfRemoteFileExistsOutput = shell.exec(`ssh ${process.env.BACKUP_MACHINE_USER}@${process.env.BACKUP_MACHINE_IP}  test -f  ${process.env.BACKUP_MACHINE_ABSOLUTE_PATH + "/" +fileName}`)
-
-if (checkIfRemoteFileExistsOutput.code === 0) {
-    shell.echo(`file ${fileName} already exists on host ${process.env.BACKUP_MACHINE_IP} user ${process.env.BACKUP_MACHINE_USER}`)
-    shell.exit(1)
-} else if (checkIfRemoteFileExistsOutput.code > 1) {
-    shell.exit(1)
-}
-
-/**
  * Syncs the backup to the remote backup host.
  */
 const rsyncOutput = shell.exec(`rsync ${path.join(__dirname, fileName)} ${process.env.BACKUP_MACHINE_USER}@${process.env.BACKUP_MACHINE_IP}:${process.env.BACKUP_MACHINE_ABSOLUTE_PATH}`)
 
 if (rsyncOutput.code !== 0) {
-    shell.echo("rsync command failed. Error: " + " " + rsyncOutput.stderr + " Output: " + " " + rsyncOutput.stdout)
     shell.exit(1)
 }
 
@@ -45,6 +31,5 @@ if (rsyncOutput.code !== 0) {
 const moveBackupOutput = shell.exec(`mv ${fileName} ${process.env.LOCAL_BACKUP_RELATIVE_PATH}`)
 
 if (moveBackupOutput.code !== 0) {
-    shell.echo("mv backup command failed. Error: " + " " + moveBackupOutput.stderr + " Output: " + " " + moveBackupOutput.stdout)
     shell.exit(1)
 }
